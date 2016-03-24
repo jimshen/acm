d1b38e17b5a5fa1d65959c36be7b4c9e243aed2b
jimshen <jimshen@gmail.com>
2016-03-24 13:53:17 +0800

    first commit

---
+++ ACM/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace ACM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String executable;
+            String arguments;
+            int progTimeLimit;
+            int testCaseTimeLimit;
+            int memoryLimit;
+            String workDir;
+            int numOfTestCases;
+            
+
+            testCaseTimeLimit = Convert.ToInt32(Console.ReadLine());
+            progTimeLimit = Convert.ToInt32(Console.ReadLine());
+            memoryLimit = Convert.ToInt32(Console.ReadLine());
+
+            String cmdLine = Console.ReadLine();
+            int pos = cmdLine.IndexOf(' ');
+            if (pos == -1)
+            {
+                executable = cmdLine;
+                arguments = "";
+            }
+            else
+            {
+                executable = cmdLine.Substring(0, pos);
+                arguments = cmdLine.Substring(pos + 1, cmdLine.Length - pos - 1);
+            }
+
+            workDir = Console.ReadLine();
+            numOfTestCases = Convert.ToInt32(Console.ReadLine());
+
+            String[] inFiles = new String[numOfTestCases];
+            String[] outFiles = new String[numOfTestCases];
+
+            for(int i=0;i<numOfTestCases;i++){
+                inFiles[i] = Console.ReadLine();
+                outFiles[i] = Console.ReadLine();
+            }
+
+            RunCmd rc = new RunCmd(executable,arguments,testCaseTimeLimit,progTimeLimit,memoryLimit,workDir,numOfTestCases,inFiles,outFiles);
+            Thread th = new Thread(rc.doWork);
+            th.Start();
+        }
+    }
+}
---
+++ ACM/RunCmd.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Threading;
+using System.IO;
+using System.Diagnostics;
+using System.Text.RegularExpressions;
+
+namespace ACM
+{
+    class RunCmd
+    {
+        private String executable;
+        private String arguments;
+        private int progTimeLimit;
+        private int testCaseTimeLimit;
+        private int memoryLimit;
+        private String workDir;
+        private int numOfTestCases;
+        private String[] inFiles;
+        private String[] outFiles;
+
+        public RunCmd(String executable, String arguments, int testCaseTimeLimit, int progTimeLimit, int memoryLimit, String workDir, int numOfTestCases, String[] inFiles, String[] outFiles)
+        {
+            this.executable = executable;
+            this.arguments = arguments;
+            this.testCaseTimeLimit = testCaseTimeLimit;
+            this.progTimeLimit = progTimeLimit;
+            this.memoryLimit = memoryLimit;
+            this.workDir = workDir;
+            this.numOfTestCases = numOfTestCases;
+            this.inFiles = inFiles;
+            this.outFiles = outFiles;
+        }
+
+        public int CompareResult(String str, int i)
+        {
+            String res = "";
+            StreamReader sr = File.OpenText(outFiles[i]);
+            while (sr.Peek() != -1)
+            {
+                string s = sr.ReadLine();
+                //Console.WriteLine(s);
+                res = res + s + "\r\n";
+            }
+            sr.Close();
+            if (String.Equals(res, str))
+                return 0;
+            String s1 = new Regex(@"[\r\n\s]*").Replace(str, "");
+            String s2 = new Regex(@"[\r\n\s]*").Replace(res, "");
+
+            if (s1.Equals(s2))
+                return 1;
+            else
+                return 4;
+        }
+
+        public void doWork()
+        {
+
+            int singleTime = 0;
+            int totalTime = 0;
+            long maxMems = -1;
+
+            if (numOfTestCases != 0)
+            {
+
+                for (int i = 0; i < numOfTestCases; i++)
+                {
+                    Process proc = new Process();
+                    proc.StartInfo.FileName = executable;
+                    proc.StartInfo.Arguments = arguments;
+                    proc.StartInfo.UseShellExecute = false;
+
+                    proc.StartInfo.RedirectStandardInput = true;
+                    proc.StartInfo.RedirectStandardOutput = true;
+                    proc.StartInfo.RedirectStandardError = true;
+                    proc.StartInfo.CreateNoWindow = false;
+
+                    string strRst = "";
+                    try
+                    {
+                        proc.Start();
+
+                        StreamWriter myStreamWriter = proc.StandardInput;
+
+
+                        StreamReader sr = File.OpenText(inFiles[i]);
+                        while (sr.Peek() != -1)
+                        {
+                            string str = sr.ReadLine();
+                            myStreamWriter.WriteLine(str + "\n");
+                        }
+                        sr.Close();
+
+
+                        long mems = proc.WorkingSet64;
+
+                        if (mems > memoryLimit)
+                        {
+                            Console.WriteLine(3);  //Time Limit Exceeded
+                            return;
+                        }
+                        if (mems > maxMems)
+                            maxMems = mems;
+
+                        Thread.Sleep(testCaseTimeLimit);
+
+                        if (proc.HasExited)
+                        {
+                            int exitCode = proc.ExitCode;
+                            strRst = proc.StandardOutput.ReadToEnd();
+                            String errmsg = proc.StandardError.ReadToEnd();
+                            if (exitCode == 0 && errmsg.Trim().Length == 0)
+                            {
+                                int x = CompareResult(strRst, i);
+                                if (x != 0)
+                                {
+                                    Console.WriteLine(x);
+                                    return;
+                                }
+                                singleTime = (proc.ExitTime - proc.StartTime).Milliseconds;
+                                totalTime = totalTime + singleTime;
+                            }
+                            else
+                            {
+                                Console.WriteLine(5);  //Run time error
+                                Console.Error.WriteLine(errmsg);
+                                Console.Error.Flush();
+                                return;
+                            }
+
+                        }
+                        else
+                        {
+                            proc.Kill();
+                            Console.WriteLine(2);  //Time Limit Exceeded!
+                            return;
+                        }
+
+
+                    }
+                    catch (OutOfMemoryException ex)
+                    {
+                        Console.WriteLine(6);  //Output Limit Exceeded
+                        Console.Error.WriteLine(ex.ToString());
+                        Console.Error.Flush();
+                        return;
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine(98);  //system error
+                        Console.Error.WriteLine(ex.ToString());
+                        Console.Error.Flush();
+                        return;
+                    }
+                    finally
+                    {
+                        if (!proc.HasExited)
+                            proc.Close();
+                    }
+                }
+                if (totalTime > progTimeLimit)
+                {
+                    Console.WriteLine(3);  //Time Limit Exceeded
+                    return;
+                }
+
+                Console.WriteLine(0); //Accepted
+                Console.WriteLine(totalTime / numOfTestCases); //Time Costs
+                Console.WriteLine(maxMems / 1024); //Max Memory Usage
+            }
+            else
+            {
+
+                Process proc = new Process();
+                proc.StartInfo.FileName = executable;
+                proc.StartInfo.Arguments = arguments;
+                proc.StartInfo.UseShellExecute = false;
+
+                proc.StartInfo.RedirectStandardInput = true;
+                proc.StartInfo.RedirectStandardOutput = true;
+                proc.StartInfo.RedirectStandardError = true;
+                proc.StartInfo.CreateNoWindow = false;
+
+                string strRst = "";
+
+                try
+                {
+                    proc.Start();
+                    StreamWriter myStreamWriter = proc.StandardInput;
+
+                    long mems = proc.WorkingSet64;
+
+                    if (mems > memoryLimit)
+                    {
+                        Console.WriteLine(3);
+                        return;
+                    }
+
+                    Thread.Sleep(testCaseTimeLimit);
+
+                    if (proc.HasExited)
+                    {
+                        int exitCode = proc.ExitCode;
+                        strRst = proc.StandardOutput.ReadToEnd();
+                        String errmsg = proc.StandardError.ReadToEnd();
+                        if (exitCode == 0 && errmsg.Trim().Length == 0)
+                        {
+                            Console.WriteLine(CompareResult(strRst, 0));
+                            Console.WriteLine((proc.ExitTime - proc.StartTime).Milliseconds);
+                            Console.WriteLine(mems);
+                            Console.WriteLine(strRst);
+                        }
+                        else
+                        {
+                            Console.WriteLine(5);  //Run time error
+                            Console.Error.WriteLine(errmsg);
+                            Console.Error.Flush();
+                        }
+
+                    }
+                    else
+                    {
+                        proc.Kill();
+                        Console.WriteLine(2);  //Time Limit Exceeded!
+                    }
+
+                }
+                catch (OutOfMemoryException ex)
+                {
+                    Console.WriteLine(6);  //system error
+                    Console.Error.WriteLine(ex.ToString());
+                    Console.Error.Flush();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(98);  //system error
+                    Console.Error.WriteLine(ex.ToString());
+                    Console.Error.Flush();
+                }
+                finally
+                {
+                    if (!proc.HasExited)
+                        proc.Close();
+                }
+            }
+            //Console.ReadKey(true);
+        }
+    }
+}
